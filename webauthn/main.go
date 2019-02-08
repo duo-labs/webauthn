07528d40299fe9a77025399fcd43229d7b98bc0e
nicksteele <nsteele@duosecurity.com>
2019-02-08 11:56:40 -0500

    update godoc bylines for the 2 packages

--- webauthn/main.go
+++ webauthn/main.go
@@ -1,4 +1,6 @@
-// woop woop
+// This package contains the API functionality of the library. After creating and
+// configuring a webauthn object, users can call the object to
+// create and validate web authentication credentials.
 package webauthn
 
 import (
