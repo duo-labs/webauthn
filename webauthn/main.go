7a149e16e1913d91e6c19415a52b5e5eaad7d204
nicksteele <nsteele@duosecurity.com>
2019-02-08 12:02:47 -0500

    update docs bylines

--- webauthn/main.go
+++ webauthn/main.go
@@ -1,6 +1,3 @@
-// This package contains the API functionality of the library. After creating and
-// configuring a webauthn object, users can call the object to
-// create and validate web authentication credentials.
 package webauthn
 
 import (
