ffce53358e2cc430c602366db0c1335e52103a07
aseigler <alexseigler@hotmail.com>
2019-05-01 15:14:13 -0400

    Move SafetyNet to jwt-go and add sanity check for timestamp

--- protocol/attestation_safetynet.go
+++ protocol/attestation_safetynet.go
@@ -4,10 +4,12 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"crypto/x509"
-	"encoding/json"
+	"encoding/base64"
 	"fmt"
+	"time"
 
-	jose "gopkg.in/square/go-jose.v2"
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/mitchellh/mapstructure"
 )
 
 var safetyNetAttestationKey = "android-safetynet"
@@ -17,13 +19,13 @@ func init() {
 }
 
 type SafetyNetResponse struct {
-	Nonce                      []byte   `json:"nonce"`
-	TimestampMs                int64    `json:"timestampMs"`
-	ApkPackageName             string   `json:"apkPackageName"`
-	ApkDigestSha256            []byte   `json:"apkDigestSha256"`
-	CtsProfileMatch            bool     `json:"ctsProfileMatch"`
-	ApkCertificateDigestSha256 [][]byte `json:"apkCertificateDigestSha256"`
-	BasicIntegrity             bool     `json:"basicIntegrity"`
+	Nonce                      string        `json:"nonce"`
+	TimestampMs                int64         `json:"timestampMs"`
+	ApkPackageName             string        `json:"apkPackageName"`
+	ApkDigestSha256            string        `json:"apkDigestSha256"`
+	CtsProfileMatch            bool          `json:"ctsProfileMatch"`
+	ApkCertificateDigestSha256 []interface{} `json:"apkCertificateDigestSha256"`
+	BasicIntegrity             bool          `json:"basicIntegrity"`
 }
 
 // Thanks to @koesie10 and @herrjemand for outlining how to support this type really well
@@ -68,64 +70,67 @@ func verifySafetyNetFormat(att AttestationObject, clientDataHash []byte) (string
 		return safetyNetAttestationKey, nil, ErrAttestationFormat.WithDetails("Unable to find the SafetyNet response")
 	}
 
-	joseResponse, parseErr := jose.ParseSigned(string(response))
-	if parseErr != nil {
-		return safetyNetAttestationKey, nil, ErrAttestationFormat.WithDetails(fmt.Sprintf("Error parsing the SafetyNet response: %+v", parseErr))
-	}
-
-	// Unpack the safetynet certs for the next steps
-	certOpts := x509.VerifyOptions{
-		DNSName: "attest.android.com",
-	}
-
-	certChain, err := joseResponse.Signatures[0].Protected.Certificates(certOpts)
-
-	// Throws error if not present or can't be verified
-	// This accomplishes §8.5.5 Verify that attestationCert is issued to the hostname "attest.android.com"
-	// https://developer.android.com/training/safetynet/index.html#compat-check-response
+	token, err := jwt.Parse(string(response), func(token *jwt.Token) (interface{}, error) {
+		chain := token.Header["x5c"].([]interface{})
+		o := make([]byte, base64.StdEncoding.DecodedLen(len(chain[0].(string))))
+		n, err := base64.StdEncoding.Decode(o, []byte(chain[0].(string)))
+		cert, err := x509.ParseCertificate(o[:n])
+		return cert.PublicKey, err
+	})
 	if err != nil {
 		return safetyNetAttestationKey, nil, ErrInvalidAttestation.WithDetails(fmt.Sprintf("Error finding cert issued to correct hostname: %+v", err))
 	}
 
-	// Get cert public key from chain if present
-	leafCert := certChain[0][0]
-	certPublicKey := leafCert.PublicKey
-
-	// Get the JWT payload if the public key is successfully verified against it
-	payload, err := joseResponse.Verify(certPublicKey)
-	if err != nil {
-		return safetyNetAttestationKey, nil, ErrAttestationFormat.WithDetails(fmt.Sprintf("Error parsing the SafetyNet certificates: %+v", err))
-	}
-
 	// marshall the JWT payload into the safetynet response json
 	var safetyNetResponse SafetyNetResponse
-	err = json.Unmarshal(payload, &safetyNetResponse)
+	err = mapstructure.Decode(token.Claims, &safetyNetResponse)
 	if err != nil {
+		fmt.Println(err)
 		return safetyNetAttestationKey, nil, ErrAttestationFormat.WithDetails(fmt.Sprintf("Error parsing the SafetyNet response: %+v", err))
 	}
 
 	// §8.5.3 Verify that the nonce in the response is identical to the Base64 encoding of the SHA-256 hash of the concatenation
 	// of authenticatorData and clientDataHash.
-
 	nonceBuffer := sha256.Sum256(append(att.RawAuthData, clientDataHash...))
-	if !bytes.Equal(nonceBuffer[:], safetyNetResponse.Nonce) {
+	nonceBytes, err := base64.StdEncoding.DecodeString(safetyNetResponse.Nonce)
+	if !bytes.Equal(nonceBuffer[:], nonceBytes) || err != nil {
 		return safetyNetAttestationKey, nil, ErrInvalidAttestation.WithDetails("Invalid nonce for in SafetyNet response")
 	}
 
 	// §8.5.4 Let attestationCert be the attestation certificate (https://www.w3.org/TR/webauthn/#attestation-certificate)
-	// Done above
+	certChain := token.Header["x5c"].([]interface{})
+	l := make([]byte, base64.StdEncoding.DecodedLen(len(certChain[0].(string))))
+	n, err := base64.StdEncoding.Decode(l, []byte(certChain[0].(string)))
+	if err != nil {
+		return safetyNetAttestationKey, nil, ErrInvalidAttestation.WithDetails(fmt.Sprintf("Error finding cert issued to correct hostname: %+v", err))
+	}
+	attestationCert, err := x509.ParseCertificate(l[:n])
+	if err != nil {
+		return safetyNetAttestationKey, nil, ErrInvalidAttestation.WithDetails(fmt.Sprintf("Error finding cert issued to correct hostname: %+v", err))
+	}
 
 	// §8.5.5 Verify that attestationCert is issued to the hostname "attest.android.com"
-	// Done above
+	err = attestationCert.VerifyHostname("attest.android.com")
+	if err != nil {
+		return safetyNetAttestationKey, nil, ErrInvalidAttestation.WithDetails(fmt.Sprintf("Error finding cert issued to correct hostname: %+v", err))
+	}
 
 	// §8.5.6 Verify that the ctsProfileMatch attribute in the payload of response is true.
 	if !safetyNetResponse.CtsProfileMatch {
 		return safetyNetAttestationKey, nil, ErrInvalidAttestation.WithDetails("ctsProfileMatch attribute of the JWT payload is false")
 	}
 
+	// Verify sanity of timestamp in the payload
+	now := time.Now()
+	oneMinuteAgo := now.Add(-time.Minute)
+	t := time.Unix(safetyNetResponse.TimestampMs/1000, 0)
+	if t.After(now) {
+		return "Basic attestation with SafetyNet", nil, ErrInvalidAttestation.WithDetails("SafetyNet response with timestamp after current time")
+	} else if t.Before(oneMinuteAgo) {
+		return "Basic attestation with SafetyNet", nil, ErrInvalidAttestation.WithDetails("SafetyNet response with timestamp before one minute ago")
+	}
+
 	// §8.5.7 If successful, return implementation-specific values representing attestation type Basic and attestation
 	// trust path attestationCert.
-
-	// TODO, return the trust path attestationCert
-	return "Basic attestation with SafetyNet", nil, nil
+	return "Basic attestation with SafetyNet", certChain, nil
 }
